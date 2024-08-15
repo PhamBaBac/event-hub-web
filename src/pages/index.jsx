828c1baa3625abd7509d285a08c60cc5bfcbaa7a
PhamBaBac <bacphamba082@gmail.com>
2024-08-15 16:24:03 +0700

    init commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePages = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <a href="https://example.com">Example Link</a>
+    </div>
+  );
+};
+
+export default HomePages;
