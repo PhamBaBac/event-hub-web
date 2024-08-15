42fab2665619c9014653258d4ef86bf581da0ac9
PhamBaBac <bacphamba082@gmail.com>
2024-08-15 16:46:33 +0700

    add assetlink

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ const HomePages = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <a href="https://example.com">Example Link</a>
+      <a href="https://event-hub-client.onrender.com">Example Link</a>
     </div>
   );
 };
